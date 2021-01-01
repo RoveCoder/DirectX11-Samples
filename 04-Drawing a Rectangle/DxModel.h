78234c1375576a7170719bfbf6532cf2ade64417
Callum Anning <CallumAnning@hotmail.co.uk>
2021-01-01 00:58:56 +0000

    Added indices

---
+++ 04-Drawing a Rectangle/DxModel.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "DxRenderer.h"
+#include <vector>
+
+namespace DX
+{
+	struct Vertex
+	{
+		float x = 0;
+		float y = 0;
+		float z = 0;
+	};
+
+	class Model
+	{
+	public:
+		Model(DX::Renderer* renderer);
+		virtual ~Model() = default;
+
+		// Create device
+		void Create();
+
+		// Render the model
+		void Render();
+
+	private:
+		DX::Renderer* m_DxRenderer = nullptr;
+
+		// Vertices list
+		std::vector<DX::Vertex> m_Vertices;
+
+		// Indeices list
+		std::vector<UINT> m_Indices;
+
+		// Vertex buffer
+		ComPtr<ID3D11Buffer> m_d3dVertexBuffer = nullptr;
+
+		// Index buffer
+		ComPtr<ID3D11Buffer> m_d3dIndexBuffer = nullptr;
+	};
+}
